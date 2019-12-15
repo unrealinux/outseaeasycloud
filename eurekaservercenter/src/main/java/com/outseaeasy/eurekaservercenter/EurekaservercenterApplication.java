5e8a1db9eccc5ff8a9b1ab00a302d7fa10975ae8
unrealinux <121058689@qq.com>
2019-12-15 20:09:33 +0800

    引入Spring Security,同时更新配置

--- eurekaservercenter/src/main/java/com/outseaeasy/eurekaservercenter/EurekaservercenterApplication.java
+++ eurekaservercenter/src/main/java/com/outseaeasy/eurekaservercenter/EurekaservercenterApplication.java
@@ -1,14 +1,17 @@
 package com.outseaeasy.eurekaservercenter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@Slf4j
 public class EurekaservercenterApplication {
 
     public static void main(String[] args) {
+        log.info("eureka server start!");
         SpringApplication.run(EurekaservercenterApplication.class, args);
     }
 
--- eurekaservercenter/src/main/java/com/outseaeasy/eurekaservercenter/config/WebSecurityConfig.java
+++ eurekaservercenter/src/main/java/com/outseaeasy/eurekaservercenter/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.outseaeasy.eurekaservercenter.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,10 +8,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        log.info("eureka server start!");
         http.csrf().disable();
     }
 
